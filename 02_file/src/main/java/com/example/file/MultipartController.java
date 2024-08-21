a0d11c4ed7f54c064b1a85085b52191fd720e172
edujeeho <edujeeho@gmail.com>
2024-08-21 14:44:49 +0900

    file handling

---
+++ 02_file/src/main/java/com/example/file/MultipartController.java
@@ -0,0 +1,35 @@
+package com.example.file;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+@RestController
+public class MultipartController {
+    @PostMapping(
+            value = "/multipart",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
+    public String multipart(
+            @RequestParam("file")
+            MultipartFile multipartFile
+    ) throws IOException {
+        System.out.println(multipartFile.getOriginalFilename());
+        byte[] bytes = multipartFile.getBytes();
+        System.out.println(new String(bytes));
+
+        File file = new File("./" + multipartFile.getOriginalFilename());
+        try (OutputStream outputStream = new FileOutputStream(file)){
+            outputStream.write(bytes);
+        }
+
+        return "success";
+    }
+}
