f84cc97473bac615c709e07df52e738082c7aaee
edujeeho <edujeeho@gmail.com>
2024-08-22 13:50:26 +0900

    custom validation exception handling

--- 03_validation/src/main/java/com/example/validation/UserController.java
+++ 03_validation/src/main/java/com/example/validation/UserController.java
@@ -3,12 +3,13 @@ package com.example.validation;
 import com.example.validation.dto.NotSeriesDto;
 import com.example.validation.dto.UserDto;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -36,5 +37,21 @@ public class UserController {
         response.put("status", "200");
         response.put("message", "OK");
         return response;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, Object> handleValidationError(
+            MethodArgumentNotValidException exception
+    ) {
+        Map<String, Object> errors = new HashMap<>();
+        List<FieldError> fieldErrors =
+                exception.getFieldErrors();
+        for (FieldError error: fieldErrors) {
+            String field = error.getField();
+            String message = error.getDefaultMessage();
+            errors.put(field, message);
+        }
+        return errors;
     }
 }
